2863c06d380cad2a1f8466903120f0ed4ae03c19
Nathan Kluth <nlkluth@gmail.com>
2015-01-14 22:50:49 -0700

    starting 2d movement

--- LastMason/Assets/Scripts/Player/PlayerMovement.cs
+++ LastMason/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour 
 {
-	void Start() 
-    {
-	
-	}
-	
-	void Update() 
-    {
-	
+	public float maxSpeed;
+
+	void FixedUpdate()
+	{
+		float move = Input.GetAxis("Horizontal");
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
 	}
 }
