125004152793ba60ec2a89fe902adc2f14215073
Nathan Kluth <nlkluth@gmail.com>
2015-01-19 16:36:00 -0700

    setting up methods to take damage + gain health

--- LastMason/Assets/Scripts/Player/PlayerHealth.cs
+++ LastMason/Assets/Scripts/Player/PlayerHealth.cs
@@ -3,5 +3,29 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour 
 {
+	public int health = 100;
+	public int maxHealth;
 
+	void Update()
+	{
+		if (health > maxHealth)
+		{
+			health = maxHealth;
+		}
+
+		if (health < 0)
+		{
+			// dead!
+		}
+	}
+
+	public void TakeDamage(int amount)
+	{
+		health -= amount;
+	}
+
+	public void RestoreHealth(int amount)
+	{
+		health += amount;
+	}
 }
