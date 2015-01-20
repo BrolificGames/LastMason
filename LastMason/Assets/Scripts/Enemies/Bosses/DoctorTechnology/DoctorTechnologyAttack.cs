4aa03577118d1548a166009ae1435636b03b625c
Nathan Kluth <nlkluth@gmail.com>
2015-01-20 07:49:16 -0700

    stubbing boss scripts

---
+++ LastMason/Assets/Scripts/Enemies/Bosses/DoctorTechnology/DoctorTechnologyAttack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoctorTechnologyAttack : MonoBehaviour 
+{
+	public GameObject missile;
+
+	private void FireMissles()
+	{
+		// get a random number between 3-5
+		// shoot that many missles
+		// missiles wind towards player
+	}
+}
